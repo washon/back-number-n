e29a1a42563f6fdd0752e1711e7d23428abf7ef6
washon <shota.wa.0510@gmail.com>
2020-07-04 03:17:12 +0900

    flatのポリフィルはapi側に移動

--- common/playlistsapi.js
+++ common/playlistsapi.js
@@ -1,22 +1,5 @@
 import axios from 'axios'
 
-// flatのpolyfillがieとedgeに必要だった...
-if (!Array.prototype.flat) {
-  Array.prototype.flat = function (depth) {
-    const flattend = [];
-    (function flat(array, depth) {
-      for (const el of array) {
-        if (Array.isArray(el) && depth > 0) {
-          flat(el, depth - 1)
-        } else {
-          flattend.push(el)
-        }
-      }
-    })(this, Math.floor(depth) || 1)
-    return flattend
-  }
-}
-
 export async function getPlaylists(apiUrl, params) {
   try {
     const res = await axios.get(`${apiUrl}/playlists`, { params: params })
